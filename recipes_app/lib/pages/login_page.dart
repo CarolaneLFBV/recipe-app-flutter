a4c017db1482dc871a7ec6dcb1190ee69d1dc529
Carolane Lefebvre <lefebvrecarolane@icloud.com>
2024-03-14 14:38:50 +0100

    Initial commit

---
+++ recipes_app/lib/pages/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+  
+}
+
+// Private class outside of this file
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
